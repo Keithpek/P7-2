e5283c1781ab66d02986b849185473a73868b057
Pek Jun Teck Keith <2400991@sit.singaporetech.edu.sg>
2024-10-09 20:57:00 +0800

    Fixed main running before form input is received, and updated lists of skillsets
    Added 3 routes in flask file, 1. root route to check if flask runs , 2. Route GET to check if form is submitted, 3. Route POST to reset form submission for running program again.
    
    Updated list of skillsets to be filtered, to cater to more jobs outside of the IT sector.
    
    Removed certain comments as well.

--- maintest.py
+++ maintest.py
@@ -13,6 +13,7 @@ import nltk
 from nltk.corpus import stopwords 
 import subprocess
 import webbrowser
+import time
 
 #Comment this line if you have already downloaded the stopwords
 #nltk.download('stopwords')
@@ -72,7 +73,7 @@ def transform(soup):
         joblist.append(job)
     return joblist
 
-  def transform_job(soup):#Function to get the job description from the job details page
+def transform_job(soup):#Function to get the job description from the job details page
     div = soup.find('div', class_='description__text description__text--rich')
     if div:
         # Remove unwanted elements
@@ -144,8 +145,6 @@ def get_jobcards(config):
     print("Total job cards scraped: ", len(all_jobs))
     all_jobs = remove_duplicates(all_jobs, config)
     print("Total job cards after removing duplicates: ", len(all_jobs))
-    #all_jobs = remove_irrelevant_jobs(all_jobs, config)
-    #print("Total job cards after removing irrelevant jobs: ", len(all_jobs))
     return all_jobs
 
 
@@ -160,51 +159,63 @@ def extract_skills(text, skillset):
 
 def cleanfile():
     stop = stopwords.words('english')
-    new_words = ['show','less']
+    new_words = ['show','less','Could','not','find','Job','Description']
     stop = stop + new_words
 
     #List of skillsets
     skills = [
-        # Programming Languages
-        'Python', 'Typescript', 'Nestjs', 'JavaScript', 'Java', 'C#', 'C++', 'Go', 'Ruby', 'Swift', 'PHP', 'Rust', 'R', 'Perl',
+        # Programming & Development
+        'Python', 'Typescript', 'Java', 'JavaScript', 'C#', 'C++', 'PHP', 'Ruby', 'Swift', 'Go', 'Kotlin', 'Perl', 'Bash', 'Shell Scripting', 'PowerShell',
+        'React', 'Angular', 'Vue.js', 'Node.js', 'Express.js', 'Django', 'Flask', 'Laravel', 'Spring', '.NET', 'jQuery', 'Bootstrap', 'SASS', 'LESS',
+        'Android', 'iOS', 'React Native', 'Flutter', 'Kotlin', 'Swift', 'Xamarin',
         
-        # Web Development
-        'HTML', 'CSS', 'React', 'Angular', 'Vue.js', 'Node.js', 'Express.js',
-        
-        # Database Technologies
-        'SQL', 'NoSQL', 'MySQL', 'PostgreSQL', 'MongoDB', 'Redis', 'Elasticsearch',
-        
-        # Cloud Platforms
-        'Google Cloud Platform', 'GCP', 'AWS', 'Amazon Web Services', 'Microsoft Azure', 'IBM Cloud', 'Oracle Cloud',
-        
-        # DevOps and Automation
-        'CICD', 'Jenkins', 'Ansible', 'Terraform', 'Chef', 'Puppet', 'GitLab CI/CD',
-        
-        # Containerization and Orchestration
-        'Docker', 'Kubernetes', 'OpenShift', 'Helm',
-        
-        # Operating Systems
-        'Linux', 'Unix', 'Windows Server',
-        
-        # Networking & Security
-        'TCP/IP', 'VPN', 'Firewalls', 'Nginx', 'Apache', 'SSL/TLS', 'Encryption', 'IAM', 'OAuth',
+        # Cloud Computing
+        'AWS', 'Amazon Web Services', 'GCP', 'Google Cloud Platform', 'Microsoft Azure', 'Oracle Cloud', 'IBM Cloud', 'DigitalOcean', 'Heroku', 'Firebase',
         
         # Data Engineering & Analytics
-        'Kafka', 'Hadoop', 'Spark', 'Hive', 'Apache Airflow',
+        'SQL', 'NoSQL', 'MySQL', 'PostgreSQL', 'MongoDB', 'Cassandra', 'Redis', 'Elasticsearch', 'MariaDB', 'DynamoDB', 'BigQuery', 'Snowflake', 'Hadoop', 'Spark', 
+        'ETL', 'Informatica', 'DBT', 'Tableau', 'Power BI', 'Talend', 'Airflow',
         
-        # Machine Learning & Data Science
-        'TensorFlow', 'PyTorch', 'Scikit-learn', 'Pandas', 'Numpy', 'Keras', 'D3.js',
+        # DevOps & Automation
+        'Jenkins', 'CircleCI', 'GitLab CI', 'Travis CI', 'Bamboo', 'Terraform', 'Ansible', 'Chef', 'Puppet', 'Docker', 'Kubernetes', 'Helm', 'OpenShift',
         
-        # Other Tools/Frameworks
-        'Salesforce', 'SAP', 'Power BI', 'Tableau', 'MATLAB', 'Splunk', 'Elasticsearch'
-        ]
+        # Networking & Security
+        'TCP/IP', 'DNS', 'SSL/TLS', 'VPN', 'SSH', 'IAM', 'OAuth', 'Firewall', 'Penetration Testing', 'Vulnerability Assessment', 'GDPR', 'CEH', 'CISSP',
+        
+        # Project Management & Methodologies
+        'Agile', 'Scrum', 'Kanban', 'Lean', 'Waterfall', 'SAFe', 'PMP', 'PRINCE2', 'Six Sigma', 'Lean Six Sigma', 'CSM',
+        
+        # Business Analysis & Strategy
+        'Business Analysis', 'Process Improvement', 'Risk Management', 'SWOT Analysis', 'Stakeholder Management',
+        
+        # Finance & Accounting
+        'QuickBooks', 'Xero', 'SAP FICO', 'CPA', 'CIMA', 'Taxation', 'Auditing', 'Financial Reporting',
+        
+        # Marketing & Sales
+        'SEO', 'SEM', 'Google Analytics', 'Salesforce', 'HubSpot', 'CRM', 'Lead Generation', 'Sales Forecasting', 'Account Management', 'Shopify',
+        
+        # Creative & Design
+        'Photoshop', 'Illustrator', 'InDesign', 'Sketch', 'Figma', 'Final Cut Pro', 'Canva', 'WordPress',
+        
+        # Healthcare
+        'Medical Coding', 'EHR', 'HIPAA Compliance', 'Patient Care', 'Nursing', 'Telemedicine', 'Pharmacy Management',
+        
+        # Supply Chain & Logistics
+        'Procurement', 'Vendor Management', 'Inventory Management', 'Shipping', 'Demand Forecasting', 'Lean Manufacturing', 'ERP',
+        
+        # Customer Service
+        'Zendesk', 'Freshdesk', 'Salesforce Service Cloud', 'Complaint Resolution', 'Technical Support',
+        
+        # Education & Training
+        'Curriculum Development', 'Lesson Planning', 'Instructional Design', 'Learning Management Systems', 'Corporate Training',
+        
+        # General Soft Skills
+        'Leadership', 'Teamwork', 'Time Management', 'Communication', 'Problem-solving', 'Adaptability', 'Emotional Intelligence', 'Multitasking'
+    ]
 
     #convert excel_file into a dataframe
     excel_file = 'jobs.csv'
     df = pd.read_csv(excel_file)
-
-    #drop rows with missing values
-    df = df.dropna() 
 
     #Removes leading and trailing whitespaces
     df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x) 
@@ -234,20 +245,54 @@ def main(config_file):
             desc_soup = get_with_retry(job['job_url'], config)
             job['job_description'] = transform_job(desc_soup)
             job_list.append(job)
-            #jobs_to_add = remove_irrelevant_jobs(job_list, config)
-            df = pd.DataFrame(job_list)
+            df = pd.DataFrame(job_list) 
             df.to_csv('jobs.csv', index=False, header=True)
     exporttoexcel(all_jobs)
     cleanfile()
 
+def wait_for_flask():
+    while True:
+        try:
+            response = requests.get('http://127.0.0.1:5000/')
+            if response.status_code == 200:
+                print("Flask is running")
+                break
+        except requests.exceptions.ConnectionError:
+            print("Flask is not running yet, waiting...")
+            time.sleep(1)
 
+def wait_for_html():
+    while True:
+        try:
+            response = requests.get('http://127.0.0.1:5000/check_form')
+            if response.status_code == 200:
+                response_data = response.json()
+                if response_data.get('status') == 'success':
+                    print("Form input received.")
+                    break
+                else:
+                    print("Waiting for form input...")
+            else:
+                print("Error checking form input")
+        except requests.exceptions.ConnectionError:
+            print("Waiting for form input...")
+        time.sleep(1)
 
+def reset_form_submission():
+    try:
+        response = requests.post('http://127.0.0.1:5000/reset_form')
+        if response.status_code == 200:
+            print("Form submission reset")
+        else:
+            print("Error resetting form submission")
+    except requests.exceptions.ConnectionError:
+        print("Error connecting to server to reset form submission")
 
 if __name__ == "__main__":
     subprocess.Popen(['python', 'flask_test.py']) #Runs flask code in non-blocking way
     webbrowser.open('ui_test.html') #Runs html file
+    wait_for_flask()
+    reset_form_submission()
+    wait_for_html()
     config_file = 'config.json'  # default config file
-    if len(sys.argv) == 2:
-        config_file = sys.argv[1]
-
     main(config_file)
