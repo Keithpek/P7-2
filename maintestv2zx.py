2a6b288b743db1ce53aff1afbc6f69195def555b
Choo Zhi Xuan <2403233@sit.singaporetech.edu.sg>
2024-10-09 23:20:49 +0800

    Added a new UI, maintestv2zx, and brand logos
    Added a new UI and maintestv2zx to try

---
+++ maintestv2zx.py
@@ -0,0 +1,290 @@
+import requests
+import json
+import sys
+from bs4 import BeautifulSoup
+import time as tm
+from itertools import groupby
+import pandas as pd
+from urllib.parse import quote
+from langdetect import detect
+from langdetect.lang_detect_exception import LangDetectException
+import re
+import nltk
+from nltk.corpus import stopwords 
+import subprocess
+import webbrowser
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+
+# Uncomment this line if you have not already downloaded the stopwords
+# nltk.download('stopwords')
+
+def load_config(file_name):
+    with open(file_name) as f:
+        return json.load(f)
+
+def get_with_retry(url, config, retries=3, delay=1):
+    for i in range(retries):
+        try:
+            if len(config['proxies']) > 0:
+                r = requests.get(url, headers=config['headers'], proxies=config['proxies'], timeout=5)
+            else:
+                r = requests.get(url, headers=config['headers'], timeout=5)
+            return BeautifulSoup(r.content, 'html.parser')
+        except requests.exceptions.Timeout:
+            logging.warning(f"Timeout occurred for URL: {url}, retrying in {delay}s...")
+            tm.sleep(delay)
+        except requests.exceptions.RequestException as e:
+            logging.error(f"An error occurred while retrieving the URL: {url}, error: {e}")
+    return None
+
+def transform(soup):
+    joblist = []
+    try:
+        divs = soup.find_all('div', class_='base-search-card__info')
+    except Exception as e:
+        logging.error(f"Error parsing the page: {e}")
+        return joblist
+    for item in divs:
+        title = item.find('h3').text.strip()
+        company = item.find('a', class_='hidden-nested-link')
+        location = item.find('span', class_='job-search-card__location')
+        parent_div = item.parent
+        entity_urn = parent_div['data-entity-urn']
+        job_posting_id = entity_urn.split(':')[-1]
+        job_url = f'https://www.linkedin.com/jobs/view/{job_posting_id}/'
+
+        date_tag_new = item.find('time', class_='job-search-card__listdate--new')
+        date_tag = item.find('time', class_='job-search-card__listdate')
+        date = date_tag['datetime'] if date_tag else date_tag_new['datetime'] if date_tag_new else ''
+        job_description = ''
+        job = {
+            'title': title,
+            'company': company.text.strip().replace('\n', ' ') if company else '',
+            'location': location.text.strip() if location else '',
+            'date': date,
+            'job_url': job_url,
+            'job_description': job_description,
+        }
+        joblist.append(job)
+    return joblist
+
+def transform_job(soup):
+    div = soup.find('div', class_='description__text description__text--rich')
+    if div:
+        for element in div.find_all(['span', 'a']):
+            element.decompose()
+        for ul in div.find_all('ul'):
+            for li in ul.find_all('li'):
+                li.insert(0, '• ')  # Adding bullet points
+
+        text = div.get_text(separator='\n').strip()
+        text = re.sub(r'\n\n', '\n', text)  # Keep single line breaks
+        text = text.replace('Show less', '').replace('Show more', '').strip()
+        return text
+    else:
+        return "Could not find Job Description"
+
+def safe_detect(text):
+    try:
+        return detect(text)
+    except LangDetectException:
+        return 'en'
+
+def remove_duplicates(joblist):
+    joblist.sort(key=lambda x: (x['title'], x['company']))
+    return [next(g) for k, g in groupby(joblist, key=lambda x: (x['title'], x['company']))]
+
+def get_jobcards(config):
+    all_jobs = []
+    for k in range(0, config['rounds']):
+        for query in config['search_queries']:
+            keywords = quote(query['keywords'])
+            location = quote(query['location'])
+            for i in range(0, config['pages_to_scrape']):
+                url = f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={keywords}&location={location}&f_TPR=&f_WT={query['f_WT']}&geoId=&f_TPR={config['timespan']}&start={25 * i}"
+                soup = get_with_retry(url, config)
+                jobs = transform(soup)
+                all_jobs.extend(jobs)
+                logging.info(f"Finished scraping page: {url}")
+    logging.info(f"Total job cards scraped: {len(all_jobs)}")
+    all_jobs = remove_duplicates(all_jobs)
+    logging.info(f"Total job cards after removing duplicates: {len(all_jobs)}")
+    return all_jobs
+
+def exporttoexcel(all_jobs):
+    df = pd.DataFrame(all_jobs)
+    # Make job description more readable
+    df['job_description'] = df['job_description'].replace(r'\n', '<br>', regex=True)
+    df.to_csv('jobs.csv', index=False, header=True)
+
+def extract_skills(text, skillset):
+    pattern = '|'.join([re.escape(skill) for skill in skillset])
+    found_skills = re.findall(pattern, text, flags=re.IGNORECASE)
+    return ', '.join(set([skill.lower() for skill in found_skills]))
+
+def cleanfile():
+    stop = stopwords.words('english')
+    new_words = ['show', 'less', 'could', 'not', 'find', 'job', 'description']
+    stop.extend(new_words)
+
+def cleanfile():
+    stop = stopwords.words('english')
+    new_words = ['show', 'less', 'could', 'not', 'find', 'job', 'description']
+    stop.extend(new_words)
+
+    skills = {
+        'Programming Languages': ['Python', 'Typescript', 'Java', 'JavaScript', 'C#', 'C++', 'PHP', 
+                                  'Ruby', 'Swift', 'Go', 'Kotlin', 'Perl', 'Bash', 'Shell Scripting', 
+                                  'PowerShell'],
+
+        'Web Development': ['React', 'Angular', 'Vue.js', 'Node.js', 'Express.js', 'Django', 
+                            'Flask', 'Laravel', 'Spring', '.NET', 'jQuery', 'Bootstrap', 
+                            'SASS', 'LESS', 'WordPress'],
+
+        'Mobile Development': ['Android', 'iOS', 'React Native', 'Flutter', 'Xamarin'],
+
+        'Cloud Platforms': ['AWS', 'Google Cloud Platform', 'Microsoft Azure', 'Oracle Cloud', 
+                            'IBM Cloud', 'DigitalOcean', 'Heroku', 'Firebase'],
+
+        'Databases': ['SQL', 'NoSQL', 'MySQL', 'PostgreSQL', 'MongoDB', 'Cassandra', 'Redis', 
+                      'Elasticsearch', 'MariaDB', 'DynamoDB', 'BigQuery', 'Snowflake', 'Hadoop', 
+                      'Spark'],
+
+        'Data Analysis': ['ETL', 'Informatica', 'DBT', 'Tableau', 'Power BI', 'Talend', 'Airflow'],
+
+        'DevOps': ['Jenkins', 'CircleCI', 'GitLab CI', 'Travis CI', 'Bamboo', 'Terraform', 
+                   'Ansible', 'Chef', 'Puppet', 'Docker', 'Kubernetes', 'Helm', 'OpenShift'],
+
+        'Networking & Security': ['TCP/IP', 'DNS', 'SSL/TLS', 'VPN', 'SSH', 'IAM', 
+                                   'OAuth', 'Firewall', 'Penetration Testing', 
+                                   'Vulnerability Assessment', 'GDPR', 'CEH', 'CISSP'],
+
+        'Agile & Project Management': ['Agile', 'Scrum', 'Kanban', 'Lean', 'Waterfall', 
+                                       'SAFe', 'PMP', 'PRINCE2', 'Six Sigma', 'Lean Six Sigma', 
+                                       'CSM', 'Business Analysis'],
+
+        'Business & Financial': ['QuickBooks', 'Xero', 'SAP FICO', 'CPA', 'CIMA', 'Taxation', 
+                                 'Auditing', 'Financial Reporting'],
+
+        'Marketing': ['SEO', 'SEM', 'Google Analytics', 'Salesforce', 'HubSpot', 'CRM', 
+                      'Lead Generation', 'Sales Forecasting', 'Account Management'],
+
+        'Design': ['Photoshop', 'Illustrator', 'InDesign', 'Sketch', 'Figma', 'Final Cut Pro', 'Canva'],
+
+        'Healthcare': ['Medical Coding', 'EHR', 'HIPAA Compliance', 'Patient Care', 'Nursing', 
+                       'Telemedicine', 'Pharmacy Management'],
+
+        'Supply Chain & Management': ['Procurement', 'Vendor Management', 'Inventory Management', 
+                                       'Shipping', 'Demand Forecasting', 'Lean Manufacturing', 'ERP'],
+
+        'Customer Support': ['Zendesk', 'Freshdesk', 'Salesforce Service Cloud', 
+                             'Complaint Resolution', 'Technical Support'],
+
+        'Education': ['Curriculum Development', 'Lesson Planning', 'Instructional Design', 
+                      'Learning Management Systems', 'Corporate Training'],
+
+        'Soft Skills': ['Leadership', 'Teamwork', 'Time Management', 'Communication', 
+                        'Problem-solving', 'Adaptability', 'Emotional Intelligence', 'Multitasking'],
+
+        # Added keywords from the Agile Data Analyst job description
+        'Agile Data Analyst': ['Agile Data Analyst', 'Agile Practice', 'Data Analysis', 
+                               'Data Trends', 'Stakeholder Needs', 'Data Gathering', 
+                               'Data Mining', 'Data Cleansing', 'Data Profiling', 
+                               'Gap Analysis', 'Performance Improvement', 'Automation', 
+                               'Data Reporting', 'KPI Generation', 'Data Visualization', 
+                               'End-to-End Analytics', 'Training', 'Compliance', 'SQL', 
+                               'R', 'Power BI', 'eazyBI', 'Jira', 'Confluence', 
+                               'Qlik Sense', 'Bachelor’s Degree', 'Banking/Finance Industry', 
+                               'Data Analyst', 'Technical Requirements', 'Business Requirements', 
+                               'Analytical Skills', 'Attention to Detail', 'Communication Skills', 
+                               'Team Collaboration', 'Proactive', 'Curiosity', 'Performance Metrics', 
+                               'Audit Risks', 'Data Governance']
+    }
+
+    df = pd.read_csv('jobs.csv')
+    df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
+    df['job_description'] = df['job_description'].str.replace(url_pattern, '', regex=True)
+    df['job_description'] = df['job_description'].str.replace(r'[^a-zA-Z0-9\s]', '', regex=True)
+    df['job_description'] = df['job_description'].apply(lambda x: ' '.join([word for word in x.split() if word.lower() not in stop]))
+    
+    # Extract skills based on categories
+    def extract_all_skills(text):
+        found_skills = []
+        for category, skill_list in skills.items():
+            pattern = '|'.join([re.escape(skill) for skill in skill_list])
+            matched_skills = re.findall(pattern, text, flags=re.IGNORECASE)
+            if matched_skills:
+                found_skills.append(f"{category}: {', '.join(set(skill.lower() for skill in matched_skills))}")
+        return '; '.join(found_skills)
+
+    df['job_description'] = df['job_description'].apply(extract_all_skills)
+
+    df.to_csv('jobs_cleaned.csv', index=False)
+
+def main(config_file):
+    config = load_config(config_file)
+    all_jobs = get_jobcards(config)
+    job_list = []
+    if all_jobs:
+        for job in all_jobs:
+            desc_soup = get_with_retry(job['job_url'], config)
+            job['job_description'] = transform_job(desc_soup)
+            job_list.append(job)
+
+        df = pd.DataFrame(job_list) 
+        df.to_csv('jobs.csv', index=False, header=True)
+
+    exporttoexcel(all_jobs)
+    cleanfile()
+
+def wait_for_flask():
+    while True:
+        try:
+            response = requests.get('http://127.0.0.1:5000/')
+            if response.status_code == 200:
+                logging.info("Flask is running")
+                break
+        except requests.exceptions.ConnectionError:
+            logging.info("Flask is not running yet, waiting...")
+            tm.sleep(1)
+
+def wait_for_html():
+    while True:
+        try:
+            response = requests.get('http://127.0.0.1:5000/check_form')
+            if response.status_code == 200:
+                response_data = response.json()
+                if response_data.get('status') == 'success':
+                    logging.info("Form input received.")
+                    break
+                else:
+                    logging.info("Waiting for form input...")
+            else:
+                logging.error("Error checking form input")
+        except requests.exceptions.ConnectionError:
+            logging.info("Waiting for form input...")
+        tm.sleep(1)
+
+def reset_form_submission():
+    try:
+        response = requests.post('http://127.0.0.1:5000/reset_form')
+        if response.status_code == 200:
+            logging.info("Form submission reset")
+        else:
+            logging.error("Error resetting form submission")
+    except requests.exceptions.ConnectionError:
+        logging.error("Error connecting to server to reset form submission")
+
+if __name__ == "__main__":
+    subprocess.Popen(['python', 'flask_test.py'])
+    webbrowser.open('ui_testzx.html')
+    wait_for_flask()
+    reset_form_submission()
+    wait_for_html()
+    config_file = 'config.json'  # default config file
+    main(config_file)
